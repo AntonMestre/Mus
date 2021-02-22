c7bcf7ba5fb56836b2cbef0a9751e48165751043
Tanguy Boscals de Réals <tbdreals@gmail.com>
2021-02-22 08:41:35 +0100

    Modification du main pour l'adapter au 2v2

--- src/main/java/com/montaury/mus/Mus.java
+++ src/main/java/com/montaury/mus/Mus.java
@@ -28,7 +28,7 @@ public class Mus {
 
     if (choixModeDeJeu == 2) {
       Partie partie = new Partie(new AffichageConsoleEvenementsDeJeu(humain));
-      Partie.Resultat resultat = partie.jouer(new Opposants(humain, Joueur.ordinateur()));
+      Partie.Resultat resultat = partie.jouer(new Opposants(humain, Joueur.ordinateur(), Joueur.ordinateur(), Joueur.ordinateur()));
 
       System.out.println("Le vainqueur de la partie est " + resultat.vainqueur().nom());
     }
