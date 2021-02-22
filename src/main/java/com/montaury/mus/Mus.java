132e6ff52bd3fcf2f0bb35e7517d94a2a4667861
Tanguy Boscals de Réals <tbdreals@gmail.com>
2021-02-22 08:31:46 +0100

    Modification de la classe Opposants pour l'adapter au 2v2

--- src/main/java/com/montaury/mus/Mus.java
+++ src/main/java/com/montaury/mus/Mus.java
@@ -4,21 +4,38 @@ import com.montaury.mus.jeu.Partie;
 import com.montaury.mus.jeu.joueur.AffichageConsoleEvenementsDeJeu;
 import com.montaury.mus.jeu.joueur.Joueur;
 import com.montaury.mus.jeu.joueur.Opposants;
+
 import java.util.Scanner;
 
 public class Mus {
   public static void main(String[] args) {
+
     // On récupère le choix du mode de jeu
     int choixModeDeJeu = choixModeJeu();
 
+    // On récupère le nom du joueur et on lui crée un objet humain
     System.out.print("Entrez votre nom: ");
     String nomJoueur = new Scanner(System.in).next();
     Joueur humain = Joueur.humain(nomJoueur);
 
-    Partie partie = new Partie(new AffichageConsoleEvenementsDeJeu(humain));
-    Partie.Resultat resultat = partie.jouer(new Opposants(humain, Joueur.ordinateur()));
+    // Lancement de la partie en fonction du mode de jeu
+    if (choixModeDeJeu == 1) {
+      Partie partie = new Partie(new AffichageConsoleEvenementsDeJeu(humain));
+      Partie.Resultat resultat = partie.jouer(new Opposants(humain, Joueur.ordinateur()));
 
-    System.out.println("Le vainqueur de la partie est " + resultat.vainqueur().nom());
+      System.out.println("Le vainqueur de la partie est " + resultat.vainqueur().nom());
+    }
+
+    if (choixModeDeJeu == 2) {
+      Partie partie = new Partie(new AffichageConsoleEvenementsDeJeu(humain));
+      Partie.Resultat resultat = partie.jouer(new Opposants(humain, Joueur.ordinateur()));
+
+      System.out.println("Le vainqueur de la partie est " + resultat.vainqueur().nom());
+    }
+
+    else {
+      System.out.println("WTF");
+    }
   }
 
   private static int choixModeJeu() {
--- src/main/java/com/montaury/mus/jeu/joueur/Opposants.java
+++ src/main/java/com/montaury/mus/jeu/joueur/Opposants.java
@@ -5,33 +5,67 @@ import java.util.List;
 
 public class Opposants {
   private Joueur joueurEsku;
+  private Joueur joueurPriorite2;
+  private Joueur joueurPriorite3;
   private Joueur joueurZaku;
+
+  private boolean jeuEnEquipe;
 
   public Opposants(Joueur joueurEsku, Joueur joueurZaku) {
     this.joueurEsku = joueurEsku;
     this.joueurZaku = joueurZaku;
+
+    this.jeuEnEquipe = false;
+  }
+
+  public Opposants(Joueur joueurEsku, Joueur joueurPriorite2, Joueur joueurPriorite3, Joueur joueurZaku) {
+    this.joueurEsku = joueurEsku;
+    this.joueurPriorite2 = joueurPriorite2;
+    this.joueurPriorite3 = joueurPriorite3;
+    this.joueurZaku = joueurZaku;
+
+    this.jeuEnEquipe = true;
   }
 
   public void tourner() {
-    Joueur tmp = joueurEsku;
-    joueurEsku = joueurZaku;
-    joueurZaku = tmp;
+    if (jeuEnEquipe) {
+      Joueur tmp = joueurEsku;
+      joueurEsku = joueurPriorite2;
+      joueurPriorite2 = joueurPriorite3;
+      joueurPriorite3 = joueurZaku;
+      joueurZaku = tmp;
+    }
+
+    else {
+      Joueur tmp = joueurEsku;
+      joueurEsku = joueurZaku;
+      joueurZaku = tmp;
+    }
+
   }
 
   public Joueur joueurEsku() {
     return joueurEsku;
   }
-
+  public Joueur joueurPriorite2() { return joueurPriorite2; }
+  public Joueur joueurPriorite3() { return joueurPriorite3; }
   public Joueur joueurZaku() {
     return joueurZaku;
   }
+  public boolean jouentEnEquipe() { return jeuEnEquipe; }
 
   public Iterator<Joueur> itererDansLOrdre() {
     return new IteratorInfini(this);
   }
 
   public List<Joueur> dansLOrdre() {
-    return List.of(joueurEsku, joueurZaku);
+    if (jeuEnEquipe) {
+      return List.of(joueurEsku, joueurPriorite2, joueurPriorite3, joueurZaku);
+    }
+
+    else {
+      return List.of(joueurEsku, joueurZaku);
+    }
   }
 
   private static class IteratorInfini implements Iterator<Joueur> {
@@ -51,7 +85,29 @@ public class Opposants {
     @Override
     public Joueur next() {
       Joueur next = suivant;
-      suivant = suivant == opposants.joueurEsku ? opposants.joueurZaku : opposants.joueurEsku;
+
+      if (opposants.jeuEnEquipe) {
+        if (suivant == opposants.joueurEsku) {
+          suivant = opposants.joueurPriorite2;
+        }
+
+        if (suivant == opposants.joueurPriorite2) {
+          suivant = opposants.joueurPriorite3;
+        }
+
+        if (suivant == opposants.joueurPriorite3) {
+          suivant = opposants.joueurZaku;
+        }
+
+        if (suivant == opposants.joueurZaku) {
+          suivant = opposants.joueurEsku;
+        }
+      }
+
+      else {
+        suivant = suivant == opposants.joueurEsku ? opposants.joueurZaku : opposants.joueurEsku;
+      }
+
       return next;
     }
   }
